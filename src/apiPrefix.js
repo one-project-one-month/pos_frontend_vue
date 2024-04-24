c825908545970806f381644d9bce620d1e3b914d
Meowveloper <zwekhantaung22.tgi@gmail.com>
2024-04-25 01:38:24 +0700

    Meowveloper - Login Function, added pinia, apiPreFixFile

---
+++ src/apiPrefix.js
@@ -0,0 +1,6 @@
+/* 
+    This file is for convenient link fix in production...I don't know if this usage is good or not..
+*/
+
+
+export default "http://127.0.0.1:8000/api";
--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia';
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia).use(router);
+app.mount('#app');
